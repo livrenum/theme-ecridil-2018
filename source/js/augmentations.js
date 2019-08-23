b17c6015a2df560f1b525ebe3f8927b79827f509
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-08-23 17:07:11 -0400

    Recover augmentations code

--- source/js/augmentations.js
+++ source/js/augmentations.js
@@ -1,32 +1,58 @@
 import 'intersection-observer'
 import scrollama from 'scrollama'
 
-const scroller = scrollama()
+const scrollWrapperSelector = '.hybrilivre-augmentation__scroll-wrapper'
+const augmentationSelector = '.hybrilivre-augmentation__augmentation'
+const stepSelector = ".hybrilivre-augmentation__step-marker"
+const scroller = setupAugmentations()
 
-const scrollWrapper = document.querySelector('.scroll__wrapper')
-const textCol = scrollWrapper.querySelector('.scroll__col_text')
-const annotCol = scrollWrapper.querySelector('.scroll__col_annotation')
-const annotElem = annotCol.querySelector('.annotation')
-const steps = textCol.querySelectorAll('.annotation__reftext')
+resize()
 
+window.addEventListener("resize", resize)
 
-scroller
-  .setup({
-    step: '.scroll__col_text .annotation__reftext',
-    container: '.scroll__wrapper',
-    graphic: '.scroll__col_annotation',
-    offset: 0.5
-  })
-  .onStepEnter(handleStepEnter)
-  .onStepExit(handleStepExit)
+function setupAugmentations() {
+  return scrollama()
+    .setup({
+      container: scrollWrapperSelector,
+      step: stepSelector,
+      offset: 0.66,
+      progress: true,
+    })
+    .onStepEnter(handleStepEnter)
+}
 
+/**
+ * Handle step trigger
+ * @param {object} t trigger object
+ */
+function handleStepEnter(t) {
+  console.log("handle step enter", t);
+  resetAugmentations()
 
-function handleStepEnter() {
-  console.log('enter')
-  annotElem.classList.add('is-active')
+  var e = t.element.getAttribute("data-augmentation-before"),
+      n = t.element.getAttribute("data-augmentation-after");
+  
+  if ("up" === t.direction && e) {
+    document.querySelector('[data-augmentation-id="' + e + '"]').classList.add("is-active")
+  } else if ("down" === t.direction && n) {
+    document.querySelector('[data-augmentation-id="' + n + '"]').classList.add("is-active")
+  }
 }
 
 function handleStepExit() {
   console.log('exit')
-  annotElem.classList.remove('is-active')
+//  annotElem.classList.remove('is-active')
+}
+function resetAugmentations() {
+  var t = Array.prototype.slice.call(document.querySelectorAll(augmentationSelector))
+  
+  console.log("reset all augm", t)
+  
+  t.forEach(function(t) {
+    t.classList.remove("is-active")
+  })
+}
+
+function resize() {
+  scroller.resize()
 }
